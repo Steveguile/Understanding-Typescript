587597ab0e21a456d50f050a7576331fb68d869f
Steve Whitehead <stephenewhitehead@outlook.com>
2025-12-10 22:53:43 +0000

    Add better OO patterns

--- Chapter 13 - DragAndDropProject/src/app.ts
+++ Chapter 13 - DragAndDropProject/src/app.ts
@@ -16,6 +16,60 @@ function autobind(
 	return adjDescriptor;
 }
 
+class ProjectList {
+
+	private templateElement: HTMLTemplateElement;
+	private hostElement: HTMLDivElement;
+	private element: HTMLElement;
+	private assignedProjects: Project[] = [];
+
+	constructor(private type: 'active' | 'finished') {
+		this.templateElement = <HTMLTemplateElement>document.getElementById('project-list')!;
+		// An alternative this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
+		this.hostElement = <HTMLDivElement>document.getElementById('app')!;
+
+		const importedNode = document.importNode(this.templateElement.content, true);
+		this.element = importedNode.firstElementChild as HTMLFormElement;
+		this.element.id = `${this.type}-projects`;
+
+		projectState.addListener((projects: Project[]) => {
+			const relevantProjects = projects.filter(project => {
+				if (this.type === 'active') {
+					return project.status == Status.ACTIVE;
+				} else {
+					return project.status == Status.FINISHED;
+				}
+			});
+			this.assignedProjects = relevantProjects;
+			this.renderProjects();
+		});
+
+		this.attach();
+		this.renderContent();
+	}	
+
+	private renderProjects() {
+		const listElement = <HTMLUListElement>document.getElementById(`${this.type}-projects-list`)!;
+		listElement.innerHTML = ''; // Get rid of all existing
+		for (const projectItem of this.assignedProjects) {
+			const listItem = document.createElement('li');
+			listItem.textContent = projectItem.title;
+			listElement.appendChild(listItem);
+		}
+	}
+
+	private renderContent() {
+		const listId = `${this.type}-projects-list`;
+		this.element.querySelector('ul')!.id = listId;
+		this.element.querySelector('h2')!.textContent = this.type.toUpperCase() + ' PROJECTS';
+	}
+
+	private attach() {
+		this.hostElement.insertAdjacentElement('beforeend', this.element);
+	}
+
+}
+
 // validation
 interface Validatable {
 	value: string | number;
@@ -134,6 +188,7 @@ class ProjectInput {
 		const userInput = this.gatherUserInput();
 		if (Array.isArray(userInput)) {
 			const[title, desc, people] = userInput;
+			projectState.addProject(title, desc, people);
 			console.log(title, desc, people)
 		}
 		this.clearInputs();
@@ -149,4 +204,56 @@ class ProjectInput {
 	}
 }
 
+class ProjectState {
+	private listeners: Listener[] = [];
+	private projects: Project[] = [];
+	private static instance: ProjectState;
+
+	private constructor() {
+
+	}
+
+	public static getInstance(): ProjectState {
+		if (this.instance) {
+			return this.instance;
+		}
+		this.instance = new ProjectState();
+		return this.instance;
+	}
+
+	// Uses function type Listener
+	public addListener(listenerFunction: Listener) {
+		this.listeners.push(listenerFunction);
+	}
+
+	public addProject(title: string, description:string, numofPeople: number) {
+		const newProject = new Project(Math.random.toString(),
+			title,
+			description,
+			numofPeople,
+			Status.ACTIVE
+		);
+		this.projects.push(newProject);
+		for (const listenerFunction of this.listeners) {
+			listenerFunction(this.projects.slice()); // slice only returns copy instead of references
+		}
+	}
+}
+
+enum Status {
+	ACTIVE,
+	FINISHED
+}
+
+// Do this to avoid literal
+class Project {
+	constructor(public id: string, public title: string, public description: string, public people: number, public status: Status) {}
+}
+
+// Function type
+type Listener = (items: Project[]) => void;
+
+const projectState = ProjectState.getInstance();
 const projectInput = new ProjectInput();
+const activeProjectList = new ProjectList('active');
+const finishedProjectList = new ProjectList('finished');
